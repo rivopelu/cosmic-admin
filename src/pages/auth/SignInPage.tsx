9b8eaec52960d53252f6bc97de5b6e7c95d0e0af
rivo <rivopelu12@gmail.com>
2025-12-23 02:24:38 +0700

    feat: Introduce Card and Input UI components, update SignInPage to utilize them, and add formik and yup dependencies.

--- src/pages/auth/SignInPage.tsx
+++ src/pages/auth/SignInPage.tsx
@@ -1,8 +1,20 @@
+import { Card, CardContent } from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+
 export default function SignInPage() {
   return (
     <main className="grid grid-cols-2 min-h-screen">
-      <div>LEFT</div>
-      <div>MAIN</div>
+      <div className="w-full bg-slate-400 h-full">LEFT</div>
+      <div className="w-full h-full flex items-center justify-center">
+        <Card>
+          <CardContent>
+            <div className="grid gap-2 min-w-sm">
+              <Input type="text" placeholder="Email" />
+              <Input type="password" placeholder="Password" />
+            </div>
+          </CardContent>
+        </Card>
+      </div>
     </main>
   )
 }
